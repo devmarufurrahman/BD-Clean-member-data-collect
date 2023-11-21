5a3cc5a5427baf6c3068bb6ac766ca9d3c4b026d
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-11-21 16:56:09 +0600

    from design complete

---
+++ app/src/main/java/com/example/bdcleanreg/FormDetails.java
@@ -0,0 +1,57 @@
+package com.example.bdcleanreg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FormDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_details);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                submitDialog("Please Fill All Fields & Submit On Time",R.drawable.warning_image,R.color.red);
+            }
+        },2000);
+
+    }
+
+
+    private void submitDialog(String notice, int imgGet, int noticeColor) {
+        Dialog dialog = new Dialog(FormDetails.this);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(true);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        TextView text = dialog.findViewById(R.id.submitTextDialog);
+        ImageView img = dialog.findViewById(R.id.submitImgDialog);
+        Button okBtn = dialog.findViewById(R.id.submitOkBtn);
+
+        text.setText(notice);
+        img.setImageResource(imgGet);
+        text.setTextColor(getResources().getColor(noticeColor));
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+
+            }
+        });
+        dialog.show();
+    }
+
+}
---
+++ app/src/main/java/com/example/bdcleanreg/Otp.java
@@ -0,0 +1,31 @@
+package com.example.bdcleanreg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Otp extends AppCompatActivity {
+
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+        loginBtn = findViewById(R.id.login_btn);
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Otp.this, FormDetails.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
